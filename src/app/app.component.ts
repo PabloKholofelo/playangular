51276a262403b34f6693c1d59c416df422c4291d
Paka Kholofelo <pkholofelo@gmail.com>
2023-03-10 10:21:26 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the GCU Cloud Test App';
+    title = "CodeSandbox";
+  private host: string= 'http://pakapokemon-env.eba-hshdfk2s.eu-north-1.elasticbeanstalk.com';
+  
+  constructor(private http: HttpClient) {
+    console.log("FD"); 
+    this.http.get<any>("https://jsonplaceholder.typicode.com/todos/1").subscribe((data: any) => {
+     console.log(data); 
+    })
+  }
 }
