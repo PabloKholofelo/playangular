b9468217c47e1e5d1909a0482edccb1a54d22a95
Paka Kholofelo <pkholofelo@gmail.com>
2023-03-12 21:29:09 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
     return this.http.get<any>(this.host + "/pokemon?offset=100&limit=100")
   }
   private getPokemonByName(name: string) {
-      // return this.http.get<any>(this.host + "/pokemon/" + name);
+      //  return this.http.get<any>(this.host + "/pokemon/" + name);
       return of({"id":5,"name":"charmeleon","sprites":{"back_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png","back_shiny":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/5.png","front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png","front_shiny":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/5.png"},"stats":[{"base_stat":"58","effort":"0","stat":{"name":"hp","url":"https://pokeapi.co/api/v2/stat/1/"}},{"base_stat":"64","effort":"0","stat":{"name":"attack","url":"https://pokeapi.co/api/v2/stat/2/"}},{"base_stat":"58","effort":"0","stat":{"name":"defense","url":"https://pokeapi.co/api/v2/stat/3/"}},{"base_stat":"80","effort":"1","stat":{"name":"special-attack","url":"https://pokeapi.co/api/v2/stat/4/"}},{"base_stat":"65","effort":"0","stat":{"name":"special-defense","url":"https://pokeapi.co/api/v2/stat/5/"}},{"base_stat":"80","effort":"1","stat":{"name":"speed","url":"https://pokeapi.co/api/v2/stat/6/"}}]})
      
   }
