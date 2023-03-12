36fe254816ef0cd60fd3abc9ea3257dc04f3f6f2
Paka Kholofelo <pkholofelo@gmail.com>
2023-03-12 17:37:59 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,21 @@ import { switchMap } from 'rxjs/operators';
 import { forkJoin, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'the GCU Cloud Test App';
-  private host: string= 'http://pakapokemon-env.eba-hshdfk2s.eu-north-1.elasticbeanstalk.com';
+  private host: string= 'https://pokeapi.co/';
   
   constructor(private http: HttpClient) {
 
       this.getPokemons().pipe(switchMap(pokemons => {
         const pokemonsArray$: Observable<any>[] = [];
         
-        pokemons.results.forEach((pokemon: any) => {
-          const pokemon$: Observable<any> = this.getPokemonByName(pokemon.name);
+        pokemons.forEach((pokemon: any) => {
+          const pokemon$: Observable<any> = this.getPokemonByName();
           pokemonsArray$.push(pokemon$);
         });
         
@@ -33,7 +33,7 @@ export class AppComponent {
   private getPokemons() {
     return this.http.get<any>(this.host + "/pokemon?offset=100&limit=100")
   }
-  private getPokemonByName(name: string) {
-      return this.http.get<any>(this.host + "/pokemon/" + name);
+  private getPokemonByName() {
+      return this.http.get<any>(this.host + "/pokemon/" + "name");
   }
 }
